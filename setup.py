f19d4fb433d05a32d7e49b3e0329e54a1e138b13
Midhun K Madhu <midhunkidangoor@gmail.com>
2023-02-03 17:36:33 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name='trajcontacts',
+    version='0.1.3',
+    scripts=['trajcontacts'] ,
+    author='Midhun K Madhu',
+    author_email='midhunkmadhu14@gmail.com',
+    description="A package to caculate residue-residue contacts from MD trajectories",
+    long_description=long_description,
+  long_description_content_type="text/markdown",
+    url='https://github.com/midhunkmadhu14/residuecontactsparent',
+    packages=setuptools.find_packages(),
+    license='BSD 2-clause',
+    install_requires=['mdtraj',
+                      'numpy',                     
+                      ],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',  
+        'Operating System :: POSIX :: Linux',        
+        'Programming Language :: Python :: 3',
+    ],
+)
